45dd2f15baaa59456d935b76c87c4498e75d4893
Varsha Jayaraman <varsha.jayaraman15@gmail.com>
2019-12-06 02:10:26 -0600

    dummy

--- upload.js
+++ upload.js
@@ -125,7 +125,7 @@ function displayTags(data, dom) {
 function recommendation(){
   alert("in recommendation")
   $.ajax({
-    type: "POST",
+    type: "GET",
     crossDomain: true,
     data: {object: "sofa"},
     // dataType: 'json',
